d58069bd8f1760cb2f34a82b82aa282880cdc2f7
shakirayoub123 <shakir.ayoub@ladybirdweb.com>
2023-08-20 23:41:13 +0530

    test

--- src/components/About.vue
+++ src/components/About.vue
@@ -18,29 +18,65 @@
           </div>
           <div class="tab-contents active-tab" id="skills">
             <ul>
-              <li><span>Programming Languages</span><br /><br>HTML, CSS, JavaScript, ReactJs, VueJs, Bootstrap, Rest Api,
-                Redux, Vuex</li>
-              <li><span>Tools</span><br /><br>Postman, IDE editors, Git, Netlify, Github, MySql, Xampp</li>
-              <!-- <li><span>Other Programming Languages</span><br/>Java (Android Studio), Python(Django)</li> -->
+              <div class="tab-content" :class="{ active: activeTab === 'skills' }">
+                <h2>Languages</h2>
+                <div class="skills-buttons">
+                  <button class="skills-button" v-for="(language, index) in programmingLanguages" :key="index"
+                    :style="{ backgroundColor: getRandomColor() }">
+                    {{ language }}
+                  </button>
+                </div>
+              </div>
+              <div class="tools-list">
+                <h2>Tools</h2>
+                <div class="skills-buttons">
+                  <button class="skills-button tool-button" v-for="(tool, index) in toolsList" :key="index"
+                    :style="{ backgroundColor: getRandomColor() }">
+                    {{ tool }}
+                  </button>
+                </div>
+              </div>
+
+
             </ul>
           </div>
           <div class="tab-contents" id="education">
             <ul>
-              <li><span>Started in 2022</span><br /><br>MERN Stack Course from Masai School</li>
-              <li><span>2016 - 2020</span><br /><br>Btech. from University of Jammu</li>
-              <li><span>Completed in 2015</span><br /><br>Intermediate in Science from State Board</li>
-              <li><span>Completed in 2013</span><br /><br>Matriculation from State Borad</li>
+              <li><span>Nov-2022 - Jan-2023</span><br /><br>MERN Stack Course from Masai School</li>
+              <li><span>2016 - 2020</span><br /><br>Strengthened my expertise with B.Tech. in Computer Science from the
+                University of Jammu.</li>
+              <li><span>2015</span><br /><br>Built my knowledge base through Science Intermediate from the State Board.
+              </li>
+              <li><span>2013</span><br /><br>Matriculation from State Board</li>
             </ul>
+
           </div>
           <div class="tab-contents" id="experience">
             <ul>
-              <li><span>Ladybird Web Solution</span><br /><br>I am currently working as a Front-end Developer at Ladybird Web
-                Solution Pvt Ltd in Jammu</li>
+              <li>
+                <strong class="skills-button">Ladybird Web Solution</strong><br /><br />
+                As a dedicated Front-end Developer at Ladybird Web Solution Pvt Ltd in Jammu, I play a pivotal role in
+                crafting captivating user experiences. My responsibilities encompass translating design concepts into
+                interactive and responsive web applications. Through my work, I contribute to creating impactful digital
+                solutions that seamlessly merge aesthetics with functionality.
+              </li>
+              <h2>Skills Learned in LWS</h2>
+              <div class="skills-buttons">
+                <button class="skills-button" v-for="(language, index) in programmingLanguages" :key="index"
+                  :style="{ backgroundColor: getRandomColor() }">
+                  {{ language }}
+                </button>
+              </div>
             </ul>
+
           </div>
           <div class="tab-contents" id="internship">
             <ul>
-              <li><span>Yarikul Infotech Srinagar</span><br /><br>I have learnt HTML, CSS, Javascript and Bootstrap from here also i have started working on my college project</li>
+              <strong class='skills-button'>Yarikul Infotech Srinagar</strong>
+              <li><br /><br>
+                I have learned HTML, CSS, JavaScript, and Bootstrap during my internship at Yarikul Infotech Srinagar.
+                I also started working on my college project, where I took the opportunity to apply the skills I gained.
+              </li>
             </ul>
           </div>
         </div>
@@ -54,7 +90,28 @@ let tablinks = document.getElementsByClassName("tab-links")
 let tabcontents = document.getElementsByClassName("tab-contents")
 export default {
   name: 'About',
+  data() {
+    return {
+      // ... other data properties
+      programmingLanguages: [
+        'HTML', 'CSS', 'JavaScript', 'ReactJs', 'VueJs', 'Bootstrap', 'Rest Api', 'Redux', 'Vuex', 'TDS'
+      ],
+      toolsList: [
+        'Postman', 'IDE editors', 'Git', 'Netlify', 'Github', 'MySql', 'Xampp'
+      ],
+      showTools: false,
+    };
+  },
+
   methods: {
+    getRandomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
     openTab(tabName, event) {
       for (let i = 0; i < tablinks.length; i++) {
         tablinks[i].classList.remove("active-link")
@@ -70,6 +127,39 @@ export default {
 </script>
 
 <style scoped>
+.tool-button {
+  background-color: #ff004f;
+}
+
+.tool-button:hover {
+  background-color: #d80045;
+}
+
+.skills-buttons {
+
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+}
+
+.skills-button {
+  background-color: #ff004f;
+  color: white;
+  border: none;
+  padding: 8px 15px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  font-size: 14px;
+  cursor: pointer;
+  transition: background-color 0.3s, transform 0.2s;
+}
+
+.skills-button:hover {
+  background-color: #d80045;
+  transform: scale(1.05);
+}
+
 #about {
   padding: 80px 0;
   color: #ababab;
@@ -142,7 +232,7 @@ export default {
     font-size: 40px;
   }
 
-  
+
 
   .row {
     display: flex;
